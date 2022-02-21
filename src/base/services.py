1fc0da468bcbc03767c401031e62d3765ed841b9
SecDet Samurai <tylerblackout17@gmail.com>
2022-02-21 15:32:30 +0300

    + added validator

--- src/base/services.py
+++ src/base/services.py
@@ -9,6 +9,13 @@ def get_path_upload_avatar(instance, file):
     return f"avatar/user_{instance.id}/{file}"
 
 
+def validate_size_image(file_object):
+    """ Проверка размера загружаемого файла """
+
+    megabyte_limit = 2
+    if file_object.size > megabyte_limit * 1024 * 1024:
+        raise ValidationError(f"Максимальный размер файла не должен превышать {megabyte_limit}MB")
+
 
 def get_path_upload_picture_album(instance, file):
     """ Построение пути к файлу, format: (media)/album/user_id/photo.jpg """
